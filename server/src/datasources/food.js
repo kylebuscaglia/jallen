91a12a7d8dbda812478bba6e77459c347e54873f
Kyle Busc <kyle.buscaglia@gmail.com>
2019-06-15 11:56:11 -0400

    implemented food endpoint

--- server/src/datasources/food.js
+++ server/src/datasources/food.js
@@ -6,18 +6,25 @@ class FoodAPI extends RESTDataSource {
     this.baseURL = 'https://api.yelp.com/v3/businesses';
   }
   
+  willSendRequest(request) {
+	request.headers.set('Authorization', 'Bearer gbHE13HIoDv6kEEE2YcgonX7kVvIOQuSUbKuAXcmsvt3VDnRy3N9fSkP17NaJYlp46n3fWSgot5qao8LRLNMV1U45pQSRWunjk9_RHq_ItTlpheRXjlG3VI42AQFXXYx');
+  }
+  
 foodReducer(food) {
   return {
-    id: food.businesses.id,
-    name: food.businesses.name,
-    url: food.businesses.url
+    id: food.id,
+    name: food.name,
+    url: food.url
   };
 }
 
 async getAllFood(zip) {
-  const response = await this.get('search?location=' + zip);
-  return Array.isArray(response)
-    ? response.map(food => this.foodReducer(food))
+  
+  var offset = Math.floor((Math.random() * 30) + 1);
+  const response = await this.get('search?limit=1&offset=' + offset + '&location=' + zip);
+  
+  return Array.isArray(response.businesses)
+    ? response.businesses.map(food => this.foodReducer(food))
     : [];
 }
 
